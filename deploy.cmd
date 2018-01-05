22471a34fc26b35c971af338f943952af1b58285
Andrew Binstead <andrew@section9.co>
2018-01-05 17:59:31 +0000

    Make sure the right version of typescript found

--- deploy.cmd
+++ deploy.cmd
@@ -108,6 +108,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
   ::call :ExecuteCmd !NPM_CMD! install --production
   call :ExecuteCmd !NPM_CMD! install
+  call :ExecuteCmd !NPM_CMD! install typescript@'>=2.1.0 <2.4.0'
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
