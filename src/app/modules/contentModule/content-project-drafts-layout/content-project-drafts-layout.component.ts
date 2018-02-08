17e466a20761865d83c4d077873e574e0db543b8
Andrew Binstead <andrew@section9.co>
2018-02-08 17:28:42 +0000

    Working draft search

--- src/app/modules/contentModule/content-project-drafts-layout/content-project-drafts-layout.component.ts
+++ src/app/modules/contentModule/content-project-drafts-layout/content-project-drafts-layout.component.ts
@@ -5,6 +5,8 @@ import { ToastsManager } from 'ng2-toastr/src/toast-manager';
 import { Router } from '@angular/router';
 import { ContentProjectShareService } from 'app/modules/contentModule/services/ContentProjectShareService';
 import { ContentItemService, ContentItemModel } from 'services/content-item.service';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
     moduleId: module.id,
@@ -14,7 +16,10 @@ import { ContentItemService, ContentItemModel } from 'services/content-item.serv
 })
 export class ContentProjectDraftsLayoutComponent implements OnInit {
     drafts: ContentItemModel[] = [];
+    draftsSub: Subscription;
     isLoading = false;
+    searchCriteria = '';
+    isFilteringSearch = false;
 
     constructor(private tracking: MixpanelService, private toast: ToastsManager, private router: Router,
         private sharedData: ContentProjectShareService, private contentItemService: ContentItemService) {
@@ -28,16 +33,13 @@ export class ContentProjectDraftsLayoutComponent implements OnInit {
             response => this.isLoading = response
         );
 
-        // Subscribe to drafts
-        this.sharedData.drafts.subscribe(
-            response => this.drafts = response
-        );
+        this.subscribeToAllDrafts();
 
         // Load the drafts
         this.sharedData.lazyLoadDrafts();
     }
 
-    AddDraft(){
+    AddDraft() {
         let currentProjectId = this.sharedData.currentProject.getValue().id;
         let url = `/content/${currentProjectId}/drafts/create`;
         console.log('Navigating to:', url);
@@ -49,5 +51,62 @@ export class ContentProjectDraftsLayoutComponent implements OnInit {
         let url = `/content/${currentProjectId}/drafts/${draft.id}`;
         console.log('Navigating to:', url);
         this.router.navigateByUrl(url);
+    }
+
+    clearSearch() {
+        this.searchCriteria = '';
+        this.isFilteringSearch = false;
+        this.subscribeToAllDrafts();
+    }
+
+    subscribeToAllDrafts() {
+        // Unsubscribe from any existing subscriptions
+        if (this.draftsSub) {
+            this.draftsSub.unsubscribe();
+        }
+
+        // Subscribe to all drafts
+        this.draftsSub = this.sharedData.drafts.subscribe(
+            response => this.drafts = response
+        );
+    }
+
+    searchDrafts(criteria: string = '') {
+        console.log('Search drafts', this.searchCriteria);
+
+        // Check to see if the call has passed in a search criteria
+        if (criteria !== '') {
+            this.searchCriteria = criteria;
+        }
+
+        // Stop listening to the old subscription
+        this.isFilteringSearch = true;
+        this.draftsSub.unsubscribe();
+
+        // Resubscribe, but filter the results
+        this.draftsSub = this.sharedData.drafts.map(
+            drafts => drafts.filter(draft => this.filterDraft(draft))).subscribe(
+            response => this.drafts = response
+            );
+    }
+
+    // Used when the user is searching to find matching drafts
+    filterDraft(draft: ContentItemModel): boolean {
+        if (draft.Title === this.searchCriteria) {
+            return true;
+        }
+
+        if (draft.Description === this.searchCriteria) {
+            return true;
+        }
+
+        if (draft.Tags) {
+            if (draft.Tags.findIndex(tag => tag === this.searchCriteria) > -1) {
+                return true;
+            }
+        }
+
+        // Draft does not match search criteria
+        return false;
     }
 }
