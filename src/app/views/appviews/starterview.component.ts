dc6a8eb75de6abc10594b425cb0ed5e4995e01c1
Andrew Binstead <andrew@section9.co>
2018-03-22 11:15:48 +0000

    Updated tour of the welcome component to include top navigation

--- src/app/views/appviews/starterview.component.ts
+++ src/app/views/appviews/starterview.component.ts
@@ -20,16 +20,41 @@ export class StarterViewComponent implements OnDestroy, OnInit {
 
     var steps = [
       {
-        element: '#tour_Welcome',
+        element: '#all',
         title: 'Welcome',
         content: 'Welcome to Otta.<br/><br/>It\'s time to take control of your content. Otta is here to help you every step of the way from creating the content to analysing the result.',
-        placement: 'bottom'
+        placement: 'bottom',
+        orphan: true
       },
       {
         element: '#tour_Agenda',
         title: 'Your agenda for the day',
         content: 'See what you need to deal with today. This is a <strong>Demo idea</strong> for the moment.',
         placement: 'left',
+        backdrop: true,
+        backdropContainer: '#wrapper'
+      },
+      {
+        element: '#help_button',
+        title: 'Get help anywhere',
+        content: 'You can get help on every page by clicking this help button',
+        placement: 'auto',
+        backdrop: true,
+        backdropContainer: '#wrapper'
+      },
+      {
+        element: '#logout_button',
+        title: 'Logout',
+        content: 'When you are finished you can just close your window, or use the logout button to log out of the system entirely',
+        placement: 'auto',
+        backdrop: true,
+        backdropContainer: '#wrapper'
+      },
+      {
+        element: '#search_bar',
+        title: 'Search',
+        content: '<strong>Not implemented yet</strong> but will eventually allow you to search all of your projects for content items, events, pitches, etc',
+        placement: 'bottom',
         backdrop: true,
         backdropContainer: '#wrapper'
       }
