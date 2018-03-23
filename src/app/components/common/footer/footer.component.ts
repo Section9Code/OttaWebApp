07e504f2a1c606afc8afbc56c8aaca7edbcd3f9f
Andrew Binstead <andrew@section9.co>
2018-03-23 17:45:57 +0000

    Added suggestion modal to page

--- src/app/components/common/footer/footer.component.ts
+++ src/app/components/common/footer/footer.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import { environment } from 'environments/environment';
 
+declare var $: any;
+
 @Component({
   selector: 'footer',
   templateUrl: 'footer.template.html'
 })
-export class FooterComponent { 
+export class FooterComponent {
   version: string = environment.version;
+  suggestionText: string;
+
+  showMakeSuggestion() {
+    $(`#makeSuggestionModal`).modal('show');
+  }
+
+  makeSuggestion(message: string) { 
+    console.log('Suggestion', message);
+    $(`#makeSuggestionModal`).modal('hide');
+  }
 }
+
