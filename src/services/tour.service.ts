2b55c5fb84e526f93da7e9530abb8b0b1ccc0775
Andrew Binstead <andrew@section9.co>
2018-09-13 14:24:05 +0100

    Updated tour service to work with URLs better

--- src/services/tour.service.ts
+++ src/services/tour.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { SweetAlertService } from 'ng2-sweetalert2';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AuthHttp } from 'angular2-jwt';
+import { environment } from 'environments/environment';
 
 /* 
     Based on Bootstrap-tour
@@ -12,41 +15,120 @@ declare var Tour: any;
 export class TourService {
     currentTourSteps: any;
     currentTour: any;
+    currentTourUrl = '';
+
+    private url: string = environment.baseApiUrl + '/api/tour';
 
     // tslint:disable-next-line:max-line-length
     popoverHtml = '<div class="popover tour-tour tour-tour-0 fade top in" role="tooltip" id="step-0" style="top: 82px; left: 811.25px; display: block;"> <div class="arrow" style="left: 50%;"></div> <h3 class="popover-title">Title of my step</h3> <div class="popover-content">Introduce new users to your product by walking them through it step by step.</div> <div class="popover-navigation"> <div class="btn-group"> <button class="btn btn-sm btn-default disabled" data-role="prev">« Prev</button> <button class="btn btn-sm btn-default" data-role="next">Next »</button>  </div> <button class="btn btn-sm btn-default" data-role="end">End tour</button> </div> </div>'
 
-    constructor(private alertSvc: SweetAlertService){}
+    constructor(
+        private router: Router,
+        private activatedRoute: ActivatedRoute,
+        private authHttp: AuthHttp,
+        private alertSvc: SweetAlertService
+    ) { }
 
     // Sets up a tour to be shown
     init(steps: any) {
-        this.currentTourSteps = steps;
-        this.currentTour = new Tour({
-            steps: this.currentTourSteps,
-            template: this.popoverHtml
-        });
+        console.warn('Tour init called - Remove this call');
+        return;
 
-        // Initialise the tour
-        this.currentTour.init();
+        // this.currentTourSteps = steps;
+        // this.currentTour = new Tour({
+        //     steps: this.currentTourSteps,
+        //     template: this.popoverHtml
+        // });
 
-        // Start the tour
-        this.currentTour.start();
+        // // Initialise the tour
+        // this.currentTour.init();
+
+        // // Start the tour
+        // this.currentTour.start();
     }
 
     // Starts a tour of the page
-    show() {
-        if (this.currentTour) {
-            // Restart the current tour
+    async show() {
+        const pageUrl = this.processUrl(this.router.url);
+        console.log(`Showing tour [${pageUrl}]`);
+
+        if (this.currentTourUrl !== pageUrl) {
+            // Load the tour for this page
+            this.currentTourUrl = pageUrl;
+            this.currentTourSteps = await this.loadSteps(this.currentTourUrl);
+            console.log('Loaded steps', this.currentTourSteps);
+
+            // Setup the tour object
+            this.currentTour = new Tour({
+                steps: this.currentTourSteps,
+                template: this.popoverHtml
+            });
+        }
+
+        // Start the tour (only if there are steps to show. Otherwise tell the user there is not help for this page yet).
+        if (this.currentTourSteps != null && this.currentTourSteps.length > 0) {
             this.currentTour.restart();
-        }
-        else {
-            // No tour to show
+        } else {
             this.alertSvc.swal('Help is coming', 'We have not setup help on this page yet, but it is coming', 'info');
+        };
+    }
+
+    // Takes the pages full URL and converts it to be used to find a guide
+    processUrl(url: string): string {
+        let parts = url.split('/');
+        let cleanUrl = '';
+
+        // Add initial slash
+        cleanUrl += '/';
+        for (let i = 1; i < parts.length; i++) {
+            let thisPart = parts[i];
+
+            // Check if this part is a number
+            if (this.isNumeric(thisPart)) { thisPart = '*'; }
+            if (this.isGuid(thisPart)) { thisPart = '*'; }
+
+            // Add the part to the url
+            cleanUrl += `${thisPart}`;
+
+            if(i !== parts.length - 1)
+            {
+                // Add trailing slash but not to the last item
+                cleanUrl += '/';
+            }
         }
+
+        return cleanUrl;
+    }
+
+    private isNumeric(value) {
+        return /^\d+$/.test(value);
+    }
+
+    private isGuid(value) {
+        const parts = value.split('-');
+        if (parts.length === 5) { return true; }
+        return false;
+    }
+
+    // Load the tour steps for the users current page
+    loadSteps(url: string): Promise<any> {
+        return new Promise<any>((resolve, reject) => {
+            // Load the steps for this url
+            this.authHttp.get(`${this.url}?path=${url}`).toPromise().then(response => {
+                // Found the steps for this tour
+                resolve(response.json());
+            })
+                .catch(() => {
+                    // Unable to find the tour for this page, return an empty tour
+                    resolve([]);
+                });
+        });
     }
 
     // Clears the current tour
     clear() {
+        console.warn('Tour clear called. Not needed');
+        this.currentTourUrl = '';
         this.currentTourSteps = null;
         this.currentTour = null;
     }
