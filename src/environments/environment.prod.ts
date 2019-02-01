7706789f47d4641c86716124a1d22629e06eb700
Andrew Binstead <andrew.binstead.uk+gitkraken@gmail.com>
2019-02-01 15:25:26 +0000

    Added production plans

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -19,5 +19,46 @@ export const environment = {
   pricePerUser: 999,
   version: 'v1.1.2',
   default_MaxRequeues: 3,
-  default_MaxProjects: 3
+  default_MaxProjects: 3,
+  plans: [
+    {
+      id: '3010',
+      optionId: '1',
+      name: 'Simplest',
+      isDefault: true,
+      maxProjects: 1,
+      maxRequeues: 3,
+      pricePerMonth: 9.95,
+      stripePricePerMonth: 995,
+      pricePerAdditionalUserPerMonth: 9.95,
+      image: '/assets/images/simplest.png',
+      description: 'Great for bloggers and small companies just starting out. With <b>1 project</b> and <b>3 requeues per project</b> you can automate your workflow without all the confusion.'
+    },
+    {
+      id: '3020',
+      optionId: '2',
+      name: 'Simpler',
+      isDefault: false,
+      maxProjects: 3,
+      maxRequeues: 4,
+      pricePerMonth: 19.95,
+      stripePricePerMonth: 1995,
+      pricePerAdditionalUserPerMonth: 9.95,
+      image: '/assets/images/simpler.png',
+      description: 'For bloggers with a lot to do or small companies with lots to keep track of. The simpler plan gives you <b>3 projects</b> and <b>4 requeues per project</b> to let you get more done.'
+    },
+    {
+      id: '3030',
+      optionId: '3',
+      name: 'Simple',
+      isDefault: false,
+      maxProjects: 6,
+      maxRequeues: 5,
+      pricePerMonth: 29.95,
+      stripePricePerMonth: 2995,
+      pricePerAdditionalUserPerMonth: 9.95,
+      image: '/assets/images/simple.png',
+      description: 'Best for medium sized companies or anyone who wants to run lots of things at once. You get <b>6 projects</b> and <b>5 requeues per project</b> to help you grow all your ideas.'
+    }
+  ]
 };
